3872f8139e8f28ac1d980d7aa1c00d658ce9ee43
Daniel Vaini <dnlvaini@gmail.com>
2021-06-02 22:51:54 -0300

    Import new component

--- react-course/react-fundaments/src/index.js
+++ react-course/react-fundaments/src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css'
 
 import First from './components/basics/First'
+import WithProperty from './components/basics/WithProperty';
 
 const tag = <strong>Olá Dan</strong>
 
 ReactDOM.render(
   <div>
-   <First />
+    <WithProperty 
+      titulo="Student situation" 
+      aluno="Dan"
+      nota={10} />
+    <First />
   </div>,
   document.getElementById('root')
-  )
+)
