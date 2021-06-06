745d3f5899105a00be6c58e6c902dfea2a2ddfd7
Daniel Vaini <dnlvaini@gmail.com>
2021-06-06 19:20:26 -0300

    Creating objects of products and students

---
+++ react-course/react-fundaments/src/data/products.js
@@ -0,0 +1,12 @@
+export default [
+  {id: 1, name: "Coca cola", priece: 2.01},
+  {id: 2, name: "Atum", priece: 2.02},
+  {id: 3, name: "Cenoura", priece: 2.03},
+  {id: 4, name: "Cebola", priece: 2.04},
+  {id: 5, name: "Chocolate", priece: 2.05},
+  {id: 6, name: "Arroz", priece: 2.06},
+  {id: 7, name: "Feijão", priece: 2.07},
+  {id: 8, name: "Tomate", priece: 2.08},
+  {id: 9, name: "Ameixa", priece: 2.09},
+  {id: 10, name: "Tinta", priece: 2.10},
+]
---
+++ react-course/react-fundaments/src/data/students.js
@@ -0,0 +1,12 @@
+export default [
+  {id: 1, nome: 'Daniel', nota: 10},
+  {id: 2, nome: 'Felipe', nota: 9},
+  {id: 3, nome: 'Bruno', nota: 8},
+  {id: 4, nome: 'Gabriel', nota: 7},
+  {id: 5, nome: 'Tania', nota: 9},
+  {id: 6, nome: 'Luciano', nota: 5.4},
+  {id: 7, nome: 'Miguel', nota: 4.6},
+  {id: 8, nome: 'Alice', nota: 3.8},
+  {id: 9, nome: 'Sophia', nota: 2.9},
+  {id: 10, nome: 'Jesus', nota: 10},
+]
