b1733dc3724abd972eca5f81da38c54f7502305e
Daniel Vaini <dnlvaini@gmail.com>
2021-06-06 19:22:38 -0300

    Even or Odd component

---
+++ react-course/react-fundaments/src/components/conditional/EvenOrOdd.jsx
@@ -0,0 +1,12 @@
+export default props => {
+  const isPair = props.number % 2 === 0
+  return (
+    <div>
+      { isPair ? 
+          <span>Pair</span> : 
+          <span>Odd</span>}
+      
+      
+    </div>
+  );
+}
