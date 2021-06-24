0faa4021b4ad26e9b4f72fd4286f2be11b9393e6
Daniel Vaini <dnlvaini@gmail.com>
2021-06-24 19:43:17 -0300

    Exercice utilizing useCallback

---
+++ react-course/hooks/src/views/examples/UseCallbackbuttons.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+const UseCallbackButtons = (props) => {
+   
+    console.log("render...")
+
+    return (
+            <div className="center">
+                <div>
+                    <button className="btn" onClick={() => props.inc(6)}>+6</button>
+                    <button className="btn" onClick={() => props.inc(12)}>+12</button>
+                    <button className="btn" onClick={() => props.inc(18)}>+18</button>
+                </div>
+            </div>
+    )
+     
+}
+
+export default React.memo(UseCallbackButtons)
