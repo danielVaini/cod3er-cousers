75b3ffe8e58c57c48e8cf515d52942e3e09ff6d4
Daniel Vaini <dnlvaini@gmail.com>
2021-07-07 22:34:12 -0300

    Creating a component for 'about' section

--- react-course/todo-app/frontend/src/about/about.jsx
+++ react-course/todo-app/frontend/src/about/about.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React from "react";
+import PageHeader from "../template/PageHeader";
 
-export default props => {
+export default (props) => {
   return (
     <div>
-      <h1>
-        About
-      </h1>
+      <PageHeader name="About" small="We" />
+      <h2>Our story</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est et suscipit
+        neque nam debitis harum molestias maiores vitae, enim repudiandae cumque
+        vel ad? Accusantium blanditiis officia quos error ab aliquam.
+      </p>
+      <h2>Mission and vision</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum
+        similique, quos esse repellendus ipsam beatae porro excepturi maxime
+        autem eum repellat possimus, fuga ea! Quibusdam voluptatum enim animi
+        iste! Ullam?
+      </p>
+      <h2>Press</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem, alias
+        vel quos quasi quibusdam nisi. Iusto maxime ratione accusantium animi,
+        nostrum non excepturi in necessitatibus eveniet, possimus voluptate
+        itaque vel!
+      </p>
     </div>
-
-  )
-}
+  );
+};
