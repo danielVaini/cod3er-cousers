58f073adbda49d044b4c768aabfbb44380c97dcb
Daniel Vaini <dnlvaini@gmail.com>
2021-07-11 17:10:55 -0300

    Utilizing components Button and grid

--- react-course/todo-app/frontend/src/todo/TodoForm.jsx
+++ react-course/todo-app/frontend/src/todo/TodoForm.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import Grid from '../template/Grid'
+import IconButton from '../template/IconButton'
 
 export default props => (
-  <div>
-    <h1>Form</h1>
+  <div role="form" className='todoForm'>
+    <Grid cols='12 9 10'>
+      <input type="text" id="description" className="form-control" placeholder=
+      "Add Task" />
+    </Grid>
+    <Grid cols='12 3 2'>
+      <IconButton style='primary' icon='plus'></IconButton>
+    </Grid>
   </div>
 )
