d6dbb14e0c09983b0e609c601225b1dadb7eeb10
Daniel Vaini <dnlvaini@gmail.com>
2021-07-11 20:20:51 -0300

    Creating event onClick

--- react-course/todo-app/frontend/src/todo/todo.jsx
+++ react-course/todo-app/frontend/src/todo/todo.jsx
@@ -4,11 +4,21 @@ import TodoForm from './TodoForm'
 import TodoList from './TodoList'
 
 export default class Todo extends Component {
+
+  constructor(props){
+    super(props)
+    this.handleAdd = this.handleAdd.bind(this)
+  }
+
+  handleAdd() {
+   
+  }
+
   render(){
     return(
       <div>
         <PageHeader name='Tasks' small='Sign up'/>
-        <TodoForm />
+        <TodoForm handleAdd={this.handleAdd}/>
         <TodoList />
       </div>
     )
