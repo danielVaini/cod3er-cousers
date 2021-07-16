e9767c37a4e433076023f500d38f2dfd6c7d64ef
Daniel Vaini <dnlvaini@gmail.com>
2021-07-15 22:25:33 -0300

    Creating all buttons and field for adding task

--- react-course/todo-app/frontend/src/todo/TodoForm.jsx
+++ react-course/todo-app/frontend/src/todo/TodoForm.jsx
@@ -1,15 +1,39 @@
-import React from 'react'
-import Grid from '../template/Grid'
-import IconButton from '../template/IconButton'
+import React from "react";
+import Grid from "../template/Grid";
+import IconButton from "../template/IconButton";
 
-export default props => (
-  <div role="form" className='todoForm'>
-    <Grid cols='12 9 10'>
-      <input type="text" id="description" className="form-control" placeholder=
-      "Add Task" />
+export default (props) => {
+
+  const keyHandler = (e) => {
+    if(e.key === 'Enter'){
+      e.shiftKey ? props.handleSearch() : props.handleAdd()
+    } else if (e.key === 'Escape') {
+      props.handleClear()
+    }
+  }
+
+  return(
+    <div role="form" className="todoForm">
+    <Grid cols="12 9 10">
+      <input
+        type="text"
+        id="description"
+        className="form-control cm-input"
+        placeholder="Add Task"
+        value={props.description}
+        onChange={props.handleChange}
+        onKeyUp={keyHandler}
+      />
     </Grid>
-    <Grid cols='12 3 2'>
-      <IconButton style='primary' icon='plus' onClick={props.handleAdd}></IconButton>
+    <Grid cols="12 3 2">
+      <IconButton
+        style="primary"
+        icon="plus"
+        onClick={props.handleAdd}
+       ></IconButton>
+      <IconButton style='info' icon='search' onClick={props.handleSearch}> </IconButton>
+      <IconButton style='default' icon='close' onClick={props.handleClear} ></IconButton>
     </Grid>
   </div>
-)
+  )
+}
