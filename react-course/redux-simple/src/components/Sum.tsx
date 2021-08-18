80697bc5c7c2fd92b9420985f784abb3ff37ae1c
Daniel Vaini <dnlvaini@gmail.com>
2021-08-17 22:57:03 -0300

    Creating the component Sum

---
+++ react-course/redux-simple/src/components/Sum.tsx
@@ -0,0 +1,16 @@
+import Card from './Card';
+
+export default function Sum() {
+  return (
+    <Card title="Sum of numbers" headerBg="#2e7d32" contenBg="#4caf50">
+      <div>
+
+        <span>
+          <span>Result: </span>
+          <strong>10</strong>
+         
+        </span>
+      </div>
+    </Card>
+  )
+}
