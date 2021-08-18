e14a1ccf9c5de6807350658364f25fe124b6dbe6
Daniel Vaini <dnlvaini@gmail.com>
2021-08-17 22:57:11 -0300

    Creating the component Raffle

---
+++ react-course/redux-simple/src/components/Raffle.tsx
@@ -0,0 +1,16 @@
+import Card from './Card';
+
+export default function Raffle() {
+  return (
+    <Card title="Raffle of one number" headerBg="#7776bC" contenBg="#7776fC">
+      <div>
+
+        <span>
+          <span>Result: </span>
+          <strong>5</strong>
+         
+        </span>
+      </div>
+    </Card>
+  )
+}
