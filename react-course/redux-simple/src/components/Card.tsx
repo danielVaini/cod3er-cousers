cae0a3dd4f63c9e467268cf4e4f4c9ed15a1771b
Daniel Vaini <dnlvaini@gmail.com>
2021-08-17 22:57:23 -0300

    Creating the component Card

--- react-course/redux-simple/src/components/Card.tsx
+++ react-course/redux-simple/src/components/Card.tsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import './Card.css'
 
+interface CardProps {
+  headerColor?: string,
+  headerBg?: string,
+  contenBg?: string,
+  children?: React.ReactNode,
+  title: string
 
+}
 
-export default (props: any) => {
+export default function Card({...props}:CardProps) {
   return (
-    <div>
-      Card
+    <div className="Card">
+      <div className="Header" style={{backgroundColor: props.headerBg}}>
+        <span className="Title" style={{color: props.headerColor || "white"}}>{props.title}</span>
+      </div>
+      <div className="Content" style={{backgroundColor: props.contenBg}}>
+        {props.children}
+      </div>
     </div>  
   )
 }
