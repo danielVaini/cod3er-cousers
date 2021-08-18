1c06a86cac8fb77bd1d3f281be8eb5153c242d51
Daniel Vaini <dnlvaini@gmail.com>
2021-08-17 22:57:33 -0300

    Creating the component Media

---
+++ react-course/redux-simple/src/components/Media.tsx
@@ -0,0 +1,16 @@
+import Card from './Card';
+
+export default function Media() {
+  return (
+    <Card title="Media of numbers" headerBg="#1565c0" contenBg="#2196f3">
+      <div>
+
+        <span>
+          <span>Result: </span>
+          <strong>5</strong>
+         
+        </span>
+      </div>
+    </Card>
+  )
+}
